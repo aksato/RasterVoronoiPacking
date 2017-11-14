a2749c323b2d18680fb2255541a3f825613f0d6a
Andre Sato <andresato@hotmail.com>
2017-11-14 15:32:45 -0200

    Added scale to Tikz output.

--- RasterVoronoiPacking/common/raster/rasterpackingsolution.cpp
+++ RasterVoronoiPacking/common/raster/rasterpackingsolution.cpp
@@ -3,6 +3,8 @@
 #include <QXmlStreamWriter>
 #include <QFile>
 
+#define TIKZ_SCALE 0.01
+
 using namespace RASTERVORONOIPACKING;
 
 RasterPackingSolution::RasterPackingSolution()
@@ -70,14 +72,14 @@ bool RasterPackingSolution::exportToPgf(QString fileName, std::shared_ptr<Raster
 	if (!file.open(QIODevice::WriteOnly)) return false;
 	QTextStream stream(&file);
 	stream << "\\begin{tikzpicture}" << endl;
-	stream << "\\draw (0,0) -- (" << length / problem->getScale() << ", 0) -- (" << length / problem->getScale() << "," << height / problem->getScale() << ") -- (0," << height / problem->getScale() << ") -- cycle;" << endl;
+	stream << "\\draw (0,0) -- (" << TIKZ_SCALE*(qreal)length / problem->getScale() << ", 0) -- (" << TIKZ_SCALE*(qreal)length / problem->getScale() << "," << TIKZ_SCALE*(qreal)height / problem->getScale() << ") -- (0," << TIKZ_SCALE*(qreal)height / problem->getScale() << ") -- cycle;" << endl;
 	for (int i = 0; i < placements.size(); i++) {
 		std::shared_ptr<RasterPackingItem> curItem = problem->getItem(i);
 		QPolygonF::iterator it = curItem->getPolygon()->begin();
-		stream << "\\draw[shift ={(" << placements.at(i).getPos().x() / problem->getScale() << "," << placements.at(i).getPos().y() / problem->getScale() << ")}, rotate=" << problem->getItem(i)->getAngleValue(placements.at(i).getOrientation()) << "] ";
-		stream << "(" << (*it).x() << "," << (*it).y() << ")";
+		stream << "\\draw[shift ={(" << TIKZ_SCALE*(qreal)placements.at(i).getPos().x() / problem->getScale() << "," << TIKZ_SCALE*(qreal)placements.at(i).getPos().y() / problem->getScale() << ")}, rotate=" << problem->getItem(i)->getAngleValue(placements.at(i).getOrientation()) << "] ";
+		stream << "(" << TIKZ_SCALE*(*it).x() << "," << TIKZ_SCALE*(*it).y() << ")";
 		for (it++; it != curItem->getPolygon()->end(); it++) {
-			stream << " -- (" << (*it).x() << "," << (*it).y() << ")";
+			stream << " -- (" << TIKZ_SCALE*(*it).x() << "," << TIKZ_SCALE*(*it).y() << ")";
 		}
 		stream << " -- cycle;" << endl;
 	}

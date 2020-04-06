3cae10b7a18e9fd80711e77043c899a332c17d45
aksato <andresato@hotmail.com>
2020-04-06 16:04:36 -0300

    Changed cuda maps to individual items (rather than by type) so that the incremental cuda works (weigh update not tested)

--- RasterVoronoiPacking/common/cuda/rasteroverlapevaluatorcudagls.cu
+++ RasterVoronoiPacking/common/cuda/rasteroverlapevaluatorcudagls.cu
@@ -11,10 +11,10 @@ RasterTotalOverlapMapEvaluatorCudaGLS::RasterTotalOverlapMapEvaluatorCudaGLS(std
 }
 
 void RasterTotalOverlapMapEvaluatorCudaGLS::populateMaps() {
-	for (int itemTypeId = 0; itemTypeId < problem->getItemTypeCount(); itemTypeId++) {
-		for (uint angle = 0; angle < (*problem->getItemByType(itemTypeId))->getAngleCount(); angle++) {
-			std::shared_ptr<TotalOverlapMapCuda> curMap = std::shared_ptr<TotalOverlapMapCuda>(new TotalOverlapMapCuda(problem->getIfps()->getRasterNoFitPolygon(0, 0, itemTypeId, angle), -1));
-			cudamaps.addOverlapMap(itemTypeId, angle, curMap);
+	for (int itemId = 0; itemId < problem->count(); itemId++) {
+		for (uint angle = 0; angle < problem->getItem(itemId)->getAngleCount(); angle++) {
+			std::shared_ptr<TotalOverlapMapCuda> curMap = std::shared_ptr<TotalOverlapMapCuda>(new TotalOverlapMapCuda(problem->getIfps()->getRasterNoFitPolygon(0, 0, problem->getItemType(itemId), angle), -1));
+			cudamaps.addOverlapMap(itemId, angle, curMap);
 			// FIXME: Delete innerift polygons as they are used to release memomry
 		}
 	}
@@ -23,15 +23,15 @@ void RasterTotalOverlapMapEvaluatorCudaGLS::populateMaps() {
 void RasterTotalOverlapMapEvaluatorCudaGLS::updateMapsLength(int pixelWidth) {
 	int deltaPixel = problem->getContainerWidth() - pixelWidth;
 	cudamaps.setShrinkVal(deltaPixel);
-	for (int itemTypeId = 0; itemTypeId < problem->getItemTypeCount(); itemTypeId++)
-		for (uint angle = 0; angle < (*problem->getItemByType(itemTypeId))->getAngleCount(); angle++) {
-			std::shared_ptr<TotalOverlapMap> curMap = cudamaps.getOverlapMap(itemTypeId, angle);
+	for (int itemId = 0; itemId < problem->count(); itemId++)
+		for (uint angle = 0; angle < problem->getItem(itemId)->getAngleCount(); angle++) {
+			std::shared_ptr<TotalOverlapMap> curMap = cudamaps.getOverlapMap(itemId, angle);
 			curMap->setRelativeWidth(deltaPixel);
 		}
 }
 
 std::shared_ptr<TotalOverlapMap> RasterTotalOverlapMapEvaluatorCudaGLS::getTotalOverlapMap(int itemId, int orientation, RasterPackingSolution &solution) {
-	std::shared_ptr<TotalOverlapMap> currrentPieceMap = cudamaps.getOverlapMap(problem->getItemType(itemId), orientation);
+	std::shared_ptr<TotalOverlapMap> currrentPieceMap = cudamaps.getOverlapMap(itemId, orientation);
 	currrentPieceMap->reset();
 	std::shared_ptr<ItemRasterNoFitPolygonSet> curItemNfpSet = problem->getNfps()->getItemRasterNoFitPolygonSet(problem->getItemType(itemId), orientation);
 	for (int i = 0; i < problem->count(); i++) {
